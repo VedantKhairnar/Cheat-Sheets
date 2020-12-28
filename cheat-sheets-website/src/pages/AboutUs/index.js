2908e144da1556b48e3d29dd7d6014c8731aa0e8
Sanjay Prajapati <sanjay270899@gmail.com>
2020-12-28 20:24:34 +0530

    🚀 Cheat Sheets Website.

---
+++ cheat-sheets-website/src/pages/AboutUs/index.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './style.scss';
+
+function AboutUs() {
+
+
+
+    return (
+        <div className="AboutUs">
+            <div className="AboutUs-title">
+                About Us
+            </div>
+        </div>
+    )
+}
+
+export default AboutUs
---
+++ cheat-sheets-website/src/pages/Landing/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.scss';
+
+function Landing() {
+    return (
+        <div className="Landing">
+            <div className="Landing-title">
+                Landing Page
+            </div>
+        </div>
+    )
+}
+
+export default Landing
